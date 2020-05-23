e3b7739d92b053eeb41038665486cb7f08ba3d58
Oscar Campo <oscampo@hotmail.com>
2020-05-22 21:30:04 -0500

    updated 2nd sesion

--- dummyLongPaso.m
+++ dummyLongPaso.m
@@ -7,7 +7,7 @@ long.L1=lpaso(leg,h);
 long.L2=lpaso(leg,h);
 long.L3=lpaso(leg,h);
 
-% long.L4=lpaso(leg,h);
+long.L4=lpaso(leg,h);
 % long.L5=lpaso(leg,h);
 % long.L6=lpaso(leg,h);
 
@@ -15,7 +15,7 @@ disp('Velocidad Normal !')
 pause;
 
 long.N1=lpaso(leg,h);
-%long.N2=lpaso(leg,h);
+long.N2=lpaso(leg,h);
 long.N3=lpaso(leg,h);
 
 % long.N4=lpaso(leg,h);
@@ -32,9 +32,9 @@ long.R3=lpaso(leg,h);
 %disp('Otras !') 
 %pause;
 
-%long.R4=lpaso(leg,h);
-
-%long.R5=lpaso(leg,h);
+% long.R4=lpaso(leg,h);
+% long.R5=lpaso(leg,h);
+% long.R6=lpaso(leg,h);
 
 files = dir('*.mat');
 save(files.name,'long','-append')
