7ca5e488d0f71c7ef189454f4e94539e225f79c7
Oscar Campo <oscampo@hotmail.com>
2020-05-26 18:41:25 -0500

    updated grafica()

--- Grafica.m
+++ Grafica.m
@@ -1,21 +1,30 @@
+clear all
 x=0:101/100:100;
-y=[Gamboa3S.L1.VelR;Gamboa3S.L2.VelR;Gamboa3S.L3.VelR];
+str1="C:\Users\oscam\OneDrive\Documentos2\GitHub\Proyecto-Helmut\DATOS";
+str2=[" PRIMERA SESION";" SEGUNDA SESION"; " TERCER SESION"];
+str3="\CAMILO GAMBOA";
+FolderPath=[ strcat(str1,str2(1),str3,str2(1));...
+strcat(str1,str2(2),str3,str2(2));...
+strcat(str1,str2(3),str3,str2(3))];
+
+for n=1:3
+cd(FolderPath(n));
+files = dir('*.mat');
+load(files.name);
+y=[L1.TetaL';L2.TetaL';L3.TetaL'];
 ymean=mean(y);
 upperBoundary = max(y);
 lowerBoundary = min(y);
 lightGray = [0.8, 0.8, 0.8];
-figure
-hold on
+%figure
+
+s(n)=subplot(3,1,n);
+grid on; 
 patch([x fliplr(x)], [upperBoundary  fliplr(lowerBoundary)], lightGray, 'LineStyle', 'none');
+hold on
 plot(x,ymean,'k-', 'LineWidth', 1)
-
-
-Gamboa3S.L1=L1;
-Gamboa3S.L2=L2;
-Gamboa3S.L3=L3;
-Gamboa3S.N1=N1;
-Gamboa3S.N2=N2;
-Gamboa3S.N3=N3;
-Gamboa3S.R1=R1;
-Gamboa3S.R2=R2;
-Gamboa3S.R3=R3;
+ylabel('Grados')
+hold off
+end
+title(s(1),{'Ángulo de Flexión de Rodilla Amputada para P1'; 'a velocidades Baja, Normal y Rápida'});
+xlabel(s(3),'Porcentaje Ciclo de Marcha')
--- PiernaAmputada.md
+++ PiernaAmputada.md
@@ -3,14 +3,14 @@ Autor: Oscar Campo
 Título: Pierna Amputada
 ---
 
-| |	Amputación	|	Edad(años)	|	Peso (Kg)	|	Altura (m)	|
-|:---|:	---	:|:	---	:|:	---	:|:	---	:|
-Camilo Gamboa	|	Izq	|	25	|	75	|	1.71	|
-Daniel Muñoz    	|	Izq	|	21	|	60	|	1.63	|
-Helmut Rubio   	|	Der	|	37	|	70	|	1.83	|
-Yulieth M. Mina    	|	Izq	|	30	|	56	|	1.64	|
-Luis Hernan Luligo    	|	Izq	|	39	|	60	|	1.73	|
-Marlon Prado     	|	Izq	|	36	|	79	|	1.82	|
-Sebastian Yacumal    	|	Der	|	44	|	76	|	1.9	|
-Prom	|		|	33.14	|	68	|	1.75	|
-SD	|		|	8.15	|	9.22	|	0.10	|
+|                  |Amputación |	Edad(años) | Peso (Kg) | Altura (m) |
+|:-----------------|:---------:|:-----------:|:---------:|:----------:|
+Camilo Gamboa	     |	Izq	  |	25	 |	75	|	1.71	|
+Daniel Muñoz       |	Izq	  |	21	 |	60	|	1.63	|
+Helmut Rubio   	   |	Der   |	37	 |	70	|	1.83	|
+Yulieth M. Mina    |	Izq	  |	30	 |	56	|	1.64	|
+Luis Hernan Luligo |	Izq	  |	39	 |	60	|	1.73	|
+Marlon Prado     	 |	Izq	  |	36	 |	79	|	1.82	|
+Sebastian Yacumal  |	Der	  |	44	 |	76	|	1.9	|
+Prom	|		         |	33.14	|	68	 |	1.75	|
+SD    |	           |	8.15	|	9.22 |	0.10	|
